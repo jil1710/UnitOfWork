7d3e6fe2dc2dc6a34a9cf945143b39e43ead5b0c
rider <jil.p@simformsolutions.com>
2023-08-11 17:34:20 +0530

    add: register serilog configuration

--- UnitOfWork/Program.cs
+++ UnitOfWork/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using UnitOfWork.Interfaces;
 using UnitOfWork.Data;
 using UnitOfWork.MiddleWare;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,8 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +27,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
