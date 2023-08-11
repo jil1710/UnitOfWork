b282172826474873bb436bddebd4f16041b66a95
rider <jil.p@simformsolutions.com>
2023-08-11 17:17:52 +0530

    add: generic repository
    Here generic repository work's with all type of entities

---
+++ UnitOfWork/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Practical_20.Interfaces
+{
+	public interface IRepository<T> where T : class
+	{
+		Task<T?> GetById(int? id);
+
+		void Insert(T entity);
+
+		void Update(T entity);
+
+		void Delete(T entity);
+
+		IEnumerable<T> GetAll();
+	}
+}
---
+++ UnitOfWork/Repository/Repository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practical_20.Data;
+using Practical_20.Interfaces;
+
+namespace Practical_20.UnitOfWork
+{
+	public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+	{
+		private DbSet<TEntity> _dbSet;
+
+		public Repository(DatabaseContext context)
+		{
+			this._dbSet = context.Set<TEntity>();
+		}
+
+		public void Delete(TEntity entity)
+		{
+			_dbSet.Remove(entity);
+		}
+
+		public IEnumerable<TEntity> GetAll()
+		{
+            return _dbSet;
+		}
+
+		public async Task<TEntity?> GetById(int? id)
+		{
+			return await _dbSet.FindAsync(id);
+		}
+
+		public void Insert(TEntity entity)
+		{
+			_dbSet.Add(entity);
+		}
+
+
+		public void Update(TEntity entity)
+		{
+			_dbSet.Update(entity);
+		}
+	}
+}
