9feb3cd4906f3db4de0db1b1b0b9c26836f7d5f5
rider <jil.p@simformsolutions.com>
2023-08-11 17:47:55 +0530

    add: home controller and view
    Landing page of applition and default route Home/Index page

---
+++ UnitOfWork/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnitOfWork.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BadRequestEx()
+        {
+            return StatusCode(400);
+        }
+
+        public IActionResult UnAuthorizeError()
+        {
+            return StatusCode(401);
+        }
+
+        public IActionResult NotFoundExample()
+        {
+            return StatusCode(404);
+        }
+
+        public IActionResult AmbiguityExample()
+        {
+            return StatusCode(300);
+        }
+
+        public IActionResult LoopEx()
+        {
+            return StatusCode(508);
+        }
+
+
+        public IActionResult InternalServerErrorEx()
+        {
+            return StatusCode(500);
+        }
+    }
+}
